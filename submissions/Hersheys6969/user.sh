1320d4a6cae33922d241fc7a23f047c115798a6a
Harsh Zope <harshzopem21@gmail.com>
2026-06-21 18:20:02 +0530

    Add files via upload

---
+++ submissions/Hersheys6969/user.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#Participant to enter their details here:
+
+name="Harsh Zope"
+Roll_no="16010125287"
+branch="Computer Engineering"
+mail="harsh.zope@somaiya.edu"
+phone="8169257580"
+remarks="Found Task2 more interesting than task1"
+#enter anything you want to add
+
+echo $name " | " $Roll_no " | " $branch " | " $remarks
+echo $mail - $phone
